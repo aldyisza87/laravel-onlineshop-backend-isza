67b0fa98c37f78afa35969b3cc5c9860af3473f5
Aldy Isza <aldyisza87@gmail.com>
2024-01-27 21:06:16 +0700

    create authcontroller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Register
+Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+
+// Logout
+Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+//Login
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
